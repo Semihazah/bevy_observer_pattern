94f42ffb9176c5b9311e55f23d006a6363d65d09
Semihazah <52551945+Semihazah@users.noreply.github.com>
2022-05-13 18:06:57 -0700

    Implemented RecieveData/GiveData for T

--- src/impls.rs
+++ src/impls.rs
@@ -1,6 +1,18 @@
-use bevy::{ui::UiImage, text::{Text, TextSection}};
+use bevy::{ui::UiImage, text::{Text, TextSection}, prelude::{Component, FromWorld}, reflect::Reflect};
 
-use crate::ReceiveData;
+use crate::{ReceiveData, GiveData};
+
+impl<T: bevy::prelude::Component + Send + Sync + 'static> ReceiveData<T> for T {
+    fn receive_data<I: Into<T>>(&mut self, data: I, _reflect_data: &dyn bevy::reflect::Reflect, _asset_server: &bevy::prelude::Res<bevy::prelude::AssetServer>, _sender: bevy::prelude::Entity) {
+        *self = data.into();
+    }
+}
+
+impl<T: Component + FromWorld + Reflect + Clone> GiveData<T> for T {
+    fn give_data(&self) -> T {
+        self.clone()
+    }
+}
 
 impl ReceiveData<String> for UiImage {
     fn receive_data<I: Into<String>>(&mut self, data: I, _reflect_data: &dyn bevy::reflect::Reflect, asset_server: &bevy::prelude::Res<bevy::prelude::AssetServer>, _sender: bevy::prelude::Entity) {
